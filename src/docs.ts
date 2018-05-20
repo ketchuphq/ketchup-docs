4419dfe610e210369ef5f226c106d4c03f10d8b9
tp <tp@octavore.com>
2018-05-20 09:53:54 -0700

    Formatting.

--- src/docs.ts
+++ src/docs.ts
@@ -1,13 +1,13 @@
 declare var hljs: any;
 import * as m from 'mithril';
-import { ClassComponent, CVnode, VnodeDOM } from 'mithril';
+import {ClassComponent, CVnode, VnodeDOM} from 'mithril';
 
 interface Page {
-  content: string
+  content: string;
 }
 
 interface ContentAttrs {
-  page: Page
+  page: Page;
 }
 
 let initialContent: Page;
@@ -15,9 +15,9 @@ let initialContent: Page;
 class ContentComponent implements ClassComponent<ContentAttrs> {
   onupdate(vnode: VnodeDOM<ContentAttrs, this>) {
     if (!vnode.dom) {
-      return
+      return;
     }
-    let blocks = vnode.dom.querySelectorAll('pre code')
+    let blocks = vnode.dom.querySelectorAll('pre code');
     for (var i = 0; i < blocks.length; i++) {
       hljs.highlightBlock(blocks[i]);
     }
@@ -32,12 +32,12 @@ class ContentComponent implements ClassComponent<ContentAttrs> {
       for (let i = 0; i < headings.length; i++) {
         let heading = headings[i];
         heading.id = `hh${i}`; // todo: hash the header or randomize somehow?
-        let link = document.createElement('a')
-        link.href = `#${heading.id}`
-        link.innerHTML = heading.innerHTML
-        let elem = document.createElement(heading.tagName)
-        elem.appendChild(link)
-        container.appendChild(elem)
+        let link = document.createElement('a');
+        link.href = `#${heading.id}`;
+        link.innerHTML = heading.innerHTML;
+        let elem = document.createElement(heading.tagName);
+        elem.appendChild(link);
+        container.appendChild(elem);
       }
     }
     return m.trust(d.innerHTML);
@@ -45,29 +45,29 @@ class ContentComponent implements ClassComponent<ContentAttrs> {
 
   view(vnode: CVnode<ContentAttrs>) {
     let page = vnode.attrs.page || initialContent;
-    return m('.content__inner__body', this.parse(page.content))
+    return m('.content__inner__body', this.parse(page.content));
   }
 }
 
 class AppComponent implements ClassComponent<void> {
   showMobileNav: boolean;
-  navPage: { content: string };
+  navPage: {content: string};
   currentPath: string;
-  currentPage: { content: string };
-  routes: { [key: string]: any };
+  currentPage: {content: string};
+  routes: {[key: string]: any};
 
   constructor() {
-    this.routes = {}
-    this.showMobileNav = false
+    this.routes = {};
+    this.showMobileNav = false;
     fetch('/api/v1/routes')
       .then((data) => data.json())
       .then((data: any) => {
         data.routes.forEach((route) => {
           this.routes[route.path] = route;
-        })
-        this.renderNav(this.routes['/nav'])
+        });
+        this.renderNav(this.routes['/nav']);
       })
-      .then(() => this.loadPage(window.location.pathname))
+      .then(() => this.loadPage(window.location.pathname));
 
     window.addEventListener('popstate', () => {
       this.loadPage(window.location.pathname, true);
@@ -79,63 +79,60 @@ class AppComponent implements ClassComponent<void> {
       .then((data) => data.json())
       .then((page: any) => {
         this.navPage = page;
-        m.redraw()
-      })
+        m.redraw();
+      });
   }
 
   loadPage(path: string, popped = false) {
-    let route = this.routes[path]
+    let route = this.routes[path];
     if (!route) {
-      document.location.href = path
-      return
+      document.location.href = path;
+      return;
     }
-    this.showMobileNav = false
-    this.currentPath = path
+    this.showMobileNav = false;
+    this.currentPath = path;
     fetch(`/api/v1/pages/${route.pageUuid}/contents`)
       .then((data) => data.json())
       .then((page: any) => {
-        this.currentPage = page
-        m.redraw()
+        this.currentPage = page;
+        m.redraw();
         if (!popped) {
-          window.history.pushState(null, '', path)
+          window.history.pushState(null, '', path);
         }
-      })
+      });
   }
 
-  handleNavClick(e: MouseEvent & { target: HTMLAnchorElement }) {
+  handleNavClick = (e: MouseEvent & {target: HTMLAnchorElement}) => {
     e.preventDefault();
     if (e.target.tagName.toLowerCase() == 'a') {
       this.loadPage(e.target.pathname);
     }
-  }
+  };
 
-  toggleNav() {
-    this.showMobileNav = !this.showMobileNav
-    m.redraw()
-  }
+  toggleNav = () => {
+    this.showMobileNav = !this.showMobileNav;
+    m.redraw();
+  };
 
   view() {
     return m('#app', [
-      m('.mobile-hamburger', { class: this.showMobileNav ? 'hidden' : '' },
-        m('a', { onclick: this.toggleNav.bind(this) }, 'Menu')
+      m(
+        '.mobile-hamburger',
+        {class: this.showMobileNav ? 'hidden' : ''},
+        m('a', {onclick: this.toggleNav}, 'Menu')
       ),
-      m('.navigation',
-        { class: this.showMobileNav ? 'navigation-mobile' : '' },
-        [
-          m('.navigation-close',
-            m('a', { onclick: this.toggleNav.bind(this) }, m.trust('&times;'))
-          ),
-          m('#nav', {
-            onclick: this.handleNavClick.bind(this)
-          }, this.navPage ? m.trust(this.navPage.content) : '')
-        ]
-      ),
-      m('.content',
-        m('.content__inner',
-          m(ContentComponent, { page: this.currentPage })
-        )
-      ),
-      m('.right-pad')
+      m('.navigation', {class: this.showMobileNav ? 'navigation-mobile' : ''}, [
+        m('.navigation-close', m('a', {onclick: this.toggleNav}, m.trust('&times;'))),
+        m(
+          '#nav',
+          {
+            onclick: this.handleNavClick,
+          },
+          this.navPage ? m.trust(this.navPage.content) : ''
+        ),
+      ]),
+      m('.content', m('.content__inner', m(ContentComponent, {page: this.currentPage}))),
+      m('.right-pad'),
     ]);
   }
 }
@@ -143,7 +140,7 @@ class AppComponent implements ClassComponent<void> {
 document.addEventListener('DOMContentLoaded', () => {
   let root = document.getElementById('app-container');
   initialContent = {
-    content: document.querySelector('.content__inner__body').innerHTML
-  }
-  m.mount(root, AppComponent)
-})
+    content: document.querySelector('.content__inner__body').innerHTML,
+  };
+  m.mount(root, AppComponent);
+});
